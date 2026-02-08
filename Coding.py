82803c7c1e943cec5b33b41f4be4e78274bd3cb2
olaxigamer-ai <olaxigamer@gmail.com>
2026-02-08 10:30:20 +0000

    Add files via upload

---
+++ Coding.py
@@ -0,0 +1,11 @@
+print("program of addition")
+num1=int(input("enter your first number."))
+num2=int(input("enter your seccond number."))
+sum=num1+num2
+minus=num1-num2
+mult=num1*num2
+div=num1/num2
+print("sum of 2 number=",sum)
+print("minus =",minus)
+print("multiplication =",mult)
+print("division =",div)
