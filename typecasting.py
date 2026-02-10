a8a53bcd617c200acd59a235561282626c9d8c3d
olaxigamer-ai <olaxigamer@gmail.com>
2026-02-10 17:25:24 +0000

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,10 @@
+b=4
+c=5.21
+print("type of b=",type(b))
+print("type of c=",type(c))
+print("data types after type casting")
+b=str(b)
+c=int(c)
+print("type of b=",type(b))
+print("type of c=",type(c))
+
