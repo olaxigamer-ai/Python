dd76de0f49b31002ae3ab874ede84710ff1f9815
olaxigamer-ai <olaxigamer@gmail.com>
2026-02-14 10:42:45 +0000

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,11 @@
+print("**************************************")
+print("calculate percentage.")
+print("**************************************")
+math=int(input("enter the marks of math="))
+cs=int(input("enter the marks of Computer Science="))
+english=int(input("enter the marks of english="))
+total=math+cs+english
+percentage=(total/300)*100
+print("the total marks of the three are",total)
+print("the percentage is",percentage)
+print("**************************************")
