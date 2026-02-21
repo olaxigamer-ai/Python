182bde8716850abb72a9059e4bdcc0ba59292dcd
olaxigamer-ai <olaxigamer@gmail.com>
2026-02-21 10:27:28 +0000

    Add files via upload

---
+++ not.py
@@ -0,0 +1,17 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+x="hello"
+y="hi"
+print(x!=y)
+d=4
+e=5
+if (d==1) != (e==5):
+    print("hello")
+f=int(input("enter a number"))
+if (f%2 != 0):
+    print("number is odd")
+else:
+    print("number is even")
