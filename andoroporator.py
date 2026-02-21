da95f184954725a60f5060d381ddd259afecc736
olaxigamer-ai <olaxigamer@gmail.com>
2026-02-21 10:17:13 +0000

    Add files via upload

---
+++ andoroporator.py
@@ -0,0 +1,15 @@
+x=10
+y=20
+
+if (x>10) or (y>10):
+    print("Either value is greater than 10")
+else:
+    print("y is greater than 10")
+
+a=10
+b=10
+
+if (a>0) and (b>0):
+    print("both values are greater than zero.")
+else:
+    print("No  value is greater than zero.")
