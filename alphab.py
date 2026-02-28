47ca3de45ba9613718adaeed829d91914e69a4c0
olaxigamer-ai <olaxigamer@gmail.com>
2026-02-28 16:59:11 +0000

    Add files via upload

---
+++ alphab.py
@@ -0,0 +1,6 @@
+string=input("Enter your string: ")
+
+if string.isalpha():
+    print("The string only has characters in the alphabet.")
+else:
+    print("The string does not only have charachters in the alphabet")
