d326da7a7e249953019c9d462d05a9187b4684dd
olaxigamer-ai <olaxigamer@gmail.com>
2026-03-01 10:27:45 +0000

    Add files via upload

---
+++ numandenom.py
@@ -0,0 +1,8 @@
+num=float(input("enter your numerator"))
+den=float(input("enter your denominator"))
+
+if num%den==0:
+    print("your first number is divisible by second number")
+else:
+    print("your first number is not divisible by second number")
+    
