6a12417776b3fae5677bdeaf4dde06d80d579557
olaxigamer-ai <olaxigamer@gmail.com>
2026-03-01 10:55:48 +0000

    Add files via upload

---
+++ compareaverage.py
@@ -0,0 +1,12 @@
+print("Three cyclists are travelling at different speeds. Find the average and check who is below average and above.")
+
+a=int(input("What was the first cyclist's speed in kmph: "))
+b=int(input("What was the second cyclist's speed in kmph: "))
+c=int(input("What was the third cyclist's speed in kmph: "))
+average=10
+print("the average is:",average)
+
+if a<average and b<average and c<average:
+    print("all three cyclists are below the range")
+
+
