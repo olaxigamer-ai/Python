27f234ebaad88d07587df00c479fec49c87754f0
olaxigamer-ai <olaxigamer@gmail.com>
2026-03-22 10:32:39 +0000

    Add files via upload

---
+++ floydstriangle.py
@@ -0,0 +1,8 @@
+row=int(input("Enter your value : "))
+num=1
+print("Floyd triangle:")
+for i in range (1,row+1):
+    for j in range(1,i+1):
+        print(num, end=" ")
+        num=num+1
+    print()
