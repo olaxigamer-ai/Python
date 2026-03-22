70ee89c36811489d0321a7cbb29b8aff223db3e0
olaxigamer-ai <olaxigamer@gmail.com>
2026-03-22 10:50:49 +0000

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,28 @@
+row=int(input("Enter the number of rows : "))
+
+if row%2==0:
+    halfrow=int(row/2)
+else:
+    halfrow=int(row/2)+1
+#upper diamond part
+space=halfrow-1
+for i in range(1,halfrow+1):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space-1
+    num=1
+    for j in range(2*i-1):
+        print(end=str(num))
+        num=num+1
+    print()
+#lower diamond part:
+space=1
+for i in range(1,halfrow):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space+1
+    num=1
+    for j in range(1,2*(halfrow-i)):
+        print(end=str(num))
+        num=num+1
+    print()
