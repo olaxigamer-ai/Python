57dc6b29910331eafe1fee4335bd582c598afb78
olaxigamer-ai <olaxigamer@gmail.com>
2026-03-28 10:38:08 +0000

    Add files via upload

---
+++ start.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("aqua")
+board=turtle.Turtle()
+#first triangle for star:
+board.forward(200)
+board.left(120)
+board.forward(200)
+board.left(120)
+board.forward(200)
+
+board.penup()
+board.right(150)
+board.forward(100)
+
+#second triangle for star:
+board.pendown()
+board.right(90)
+board.forward(200)
+board.right(120)
+board.forward(200)
+board.right(120)
+board.forward(200)
+
+turtle.done()
