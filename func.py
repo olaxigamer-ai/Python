2a6c7bc5ec3a589af967e9df1fdd61e5e44e4092
olaxigamer-ai <olaxigamer@gmail.com>
2026-03-29 13:28:37 +0300

    Add files via upload

---
+++ func.py
@@ -0,0 +1,4 @@
+def my_first_function():
+    print("Hello")
+    print("What is your name?")
+my_first_function()
---
+++ funct2.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    c=a+b
+    return(c)
+print("total is: ", add(10,20))
+
+def id(name,grade):
+    print(name)
+    print(grade)
+id("Alex", 12)
+
+id("bob", 20)
