6b872c43aa13c7528fad494cd171a9e016bb6ada
olaxigamer-ai <olaxigamer@gmail.com>
2026-05-02 10:40:59 +0100

    Add files via upload

--- tupprod.py
+++ tupprod.py
@@ -1,4 +1,9 @@
-tup1 = (4,3,2,2,-1,18)
-tup2 = (2,4,8,8,3,2,9)
-
-print(tup1*tup2)
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+p1 = 1
+for x in tup1:
+    p1 *= x
+p2 = 1
+for x in tup2:
+    p2 *= x
+print(p1 * p2)
