1f3158e0ef3d474e029ec6cd0af13be652319a1d
olaxigamer-ai <olaxigamer@gmail.com>
2026-05-03 11:26:55 +0100

    Add files via upload

---
+++ operationsonsets.py
@@ -0,0 +1,6 @@
+set1={1,2,3,4,5,5}
+print(set1)
+set2={"hello",1.25,4,True}
+print(set2)
+set2.pop()
+print(set2)
---
+++ set1.py
@@ -0,0 +1,10 @@
+set1={2,3,5,9}
+set2={4,5,9,11}
+set3=set1.union(set2)
+print(set3)
+set4=set1.intersection(set2)
+print(set4)
+set5=set1.difference(set2)
+print(set5)
+set6=set1.symmetric_difference(set2)
+print(set6)
