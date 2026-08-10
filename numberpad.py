9e512c196681db47b98be2edd5910df8c22083a3
olaxigamer-ai <olaxigamer@gmail.com>
2026-08-10 10:01:11 +0300

    Add files via upload

--- numberpad.py
+++ numberpad.py
@@ -12,6 +12,6 @@ for i in range(4):
     for j in range(0,3):
         frame=Frame(master=root, relief=SUNKEN, borderwidth=1)
         frame.grid(row=i,column=j)
-        label=Label(master=frame, text=num[i][j],bg="blue")
+        label=Label(master=frame, text=num[i][j],bg="#d0efff")
         label.pack(padx=3,pady=3)
 root.mainloop()
